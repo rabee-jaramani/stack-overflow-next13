839973d3292e388414dfaf5c86d756f2b472bea5
Rabee Aljaramani <rabeejermani@gmail.com>
2024-01-13 12:36:43 +0400

    edit

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,10 +5,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
-import { SignedOut, useAuth } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
+
 const LeftSidebar = () => {
-  const { userId } = useAuth();
   const pathname = usePathname();
+
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
@@ -17,13 +18,7 @@ const LeftSidebar = () => {
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
 
-          if (item.route === "/profile") {
-            if (userId) {
-              item.route = `${item.route}/${userId}`;
-            } else {
-              return null;
-            }
-          }
+          // TODO
 
           return (
             <Link
